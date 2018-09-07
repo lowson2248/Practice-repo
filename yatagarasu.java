966dc41187cfc3c3a6698517a253b7924f0e35a5
lowson <iwase2248@gmail.com>
2018-09-07 13:35:32 +0900

    namae oomojini sitayo

--- yatagarasu.java
+++ yatagarasu.java
@@ -1,20 +1,18 @@
-java.util.*;
-  
 class Yatagarasu extends Monster implements Bird {
     public void run(){
         System.out.println("Birdは飛んだ");
     }
-    
+
     public int attack(int hp){
         System.out.println("攻撃");
         hp -= 20;
         return hp;
     }
-    
+
     public void dead(int hp){
         if(hp <= 0){
             System.out.println("HPが0になりました");
         }
     }
 }
-     
+
