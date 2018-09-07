0cb42c07d74ef40d4af7d046b709596c167b59f1
kohta <15.kohta@gmail.com>
2018-09-07 13:21:31 +0900

    added

--- yatagarasu.java
+++ yatagarasu.java
@@ -1,7 +1,20 @@
 java.util.*;
-
+  
 class Yatagarasu extends Monster implements Bird {
     public void run(){
         System.out.println("Birdは飛んだ");
     }
+    
+    public int attack(int hp){
+        System.out.println("攻撃");
+        hp -= 20;
+        return hp;
+    }
+    
+    public void dead(int hp){
+        if(hp <= 0){
+            System.out.println("HPが0になりました");
+        }
+    }
 }
+     
